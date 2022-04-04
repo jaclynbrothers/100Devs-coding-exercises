25711b5e8ea6e2fc8b64cdfbd451de1532670f07
Jaclyn Brothers <102431119+jaclynbrothers@users.noreply.github.com>
2022-04-03 20:10:57 -0700

    Added customized class 12 background picker exercise

---
+++ Class-12/shaq-jersey-picker/js/main.js
@@ -0,0 +1,42 @@
+document.getElementById('clevelandDarkBlue').onclick = clevelandDarkBlue
+document.getElementById('lakersBlue').onclick = lakersBlue
+document.getElementById('orlandoLighterBlue').onclick = orlandoLighterBlue
+document.getElementById('celticsGreen').onclick = celticsGreen
+document.getElementById('lakersYellow').onclick = lakersYellow
+document.getElementById('sunsOrange').onclick = sunsOrange
+document.getElementById('miamiRed').onclick = miamiRed
+
+function clevelandDarkBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgb(38,37,43)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function lakersBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgb(68,65,146)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function orlandoLighterBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgb(20,61,171)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function celticsGreen(){
+  document.querySelector('body').style.backgroundColor = 'rgb(46,141,83)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function lakersYellow(){
+  document.querySelector('body').style.backgroundColor = 'rgb(239,163,61)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function sunsOrange(){
+  document.querySelector('body').style.backgroundColor = 'rgb(255,122,17)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function miamiRed(){
+  document.querySelector('body').style.backgroundColor = 'rgb(130,15,18)'
+  document.querySelector('body').style.color = 'white'
+}
