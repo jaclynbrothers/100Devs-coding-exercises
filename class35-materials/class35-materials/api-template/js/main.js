97cce2d95757da61225c56ac7700edeed0413f96
Jaclyn Brothers <102431119+jaclynbrothers@users.noreply.github.com>
2022-05-25 21:28:51 -0700

    Added try catch statement

--- class35-materials/class35-materials/api-template/js/main.js
+++ class35-materials/class35-materials/api-template/js/main.js
@@ -1,11 +1,14 @@
 //Make an api request using async await
 
+// add a try catch block so you can catch errors
 
 async function getACuteDogPhoto(){
-    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    try{
+    const res = await fetch('httpsbroken://dog.ceo/api/breeds/image/random')
     const data = await res.json()
     console.log(data)
+    }catch (error) {
+        console.error('Catch is working: ' + error);
+    }
 }
 getACuteDogPhoto()
-
-// add a try catch block so you can catch errors
